4b9bf6fe502fdb0be7c2fcfca8d734f7991b1569
Michael <michaelodell660@gmail.com>
2021-04-24 14:45:47 -0700

    starting to implement Formik on the new lactic-acid fermentation page

--- src/pages/NewLactoferment.js
+++ src/pages/NewLactoferment.js
@@ -1,83 +1,199 @@
 import React, { useState, useRef } from 'react';
+import { Formik, Field, FieldArray } from 'formik';
 
 export default function NewLactoFerment(props) {
-	const [lactoferments, setlactoFerments] = useState([]);
+	// const [lactoferments, setlactoFerments] = useState([]);
 
-	const volumeVal = useRef(null);
-	const volumeUnit = useRef(null);
-	const fermentTemp = useRef(null);
-	const fermentDuration = useRef(null);
-	const saltAmount = useRef(null);
-	const saltUnit = useRef(null);
-	const mainName = useRef(null);
-	const mainUnit = useRef(null);
-	const mainValue = useRef(null);
-	const otherName = useRef(null);
-	const otherValue = useRef(null);
-	const otherUnit = useRef(null);
-	const name = useRef(null);
-	const description = useRef(null);
+	// const volumeVal = useRef(null);
+	// const volumeUnit = useRef(null);
+	// const fermentTemp = useRef(null);
+	// const fermentDuration = useRef(null);
+	// const saltAmount = useRef(null);
+	// const saltUnit = useRef(null);
+	// const mainName = useRef(null);
+	// const mainUnit = useRef(null);
+	// const mainValue = useRef(null);
+	// const otherName = useRef(null);
+	// const otherValue = useRef(null);
+	// const otherUnit = useRef(null);
+	// const name = useRef(null);
+	// const description = useRef(null);
 
-	const handleSubmit = async e => {
-		e.preventDefault();
+	// const handleSubmit = async e => {
+	// 	e.preventDefault();
 
-		try {
-			const response = await fetch('/api/lactoferments', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					name: name.current.value,
-					description: description.current.value,
+	// 	try {
+	// 		const response = await fetch('/api/lactoferments', {
+	// 			method: 'POST',
+	// 			headers: {
+	// 				'Content-Type': 'application/json'
+	// 			},
+	// 			body: JSON.stringify({
+	// 				name: name.current.value,
+	// 				description: description.current.value,
+	// 				volume: {
+	// 					value: volumeVal.current.value,
+	// 					unit: volumeUnit.current.value
+	// 				},
+	// 				method: {
+	// 					ferment: {
+	// 						temp: fermentTemp.current.value,
+	// 						duration: fermentDuration.current.value
+	// 					}
+	// 				},
+	// 				ingredients: {
+	// 					salt: {
+	// 						amount: {
+	// 							value: saltAmount.current.value,
+	// 							unit: saltUnit.current.value
+	// 						}
+	// 					},
+	// 					main: [
+	// 						{
+	// 							amount: {
+	// 								value: mainValue.current.value,
+	// 								unit: mainUnit.current.value
+	// 							},
+	// 							name: mainName.current.value
+	// 						}
+	// 					],
+	// 					other: [
+	// 						{
+	// 							amount: {
+	// 								value: otherValue.current.value,
+	// 								unit: otherUnit.current.value
+	// 							},
+	// 							name: otherName.current.value
+	// 						}
+	// 					]
+	// 				}
+	// 			})
+	// 		});
+	// 		setlactoFerments([data, ...lactoferments]);
+	// 	} catch (error) {
+	// 		console.error(error);
+	// 	} finally {
+	// 		window.location.assign('/');
+	// 	}
+	// };
+	return (
+		<div className="newLactofermentForm">
+			<h1>New Fermentation</h1>
+			<Formik
+				initialValues={{
+					name: '',
+					description: '',
 					volume: {
-						value: volumeVal.current.value,
-						unit: volumeUnit.current.value
+						value: 0.0,
+						unit: 'unit'
 					},
 					method: {
 						ferment: {
-							temp: fermentTemp.current.value,
-							duration: fermentDuration.current.value
+							temp: "20 C",
+							duration: 'weeks'
 						}
 					},
+					// added inputs for up to here
 					ingredients: {
 						salt: {
 							amount: {
-								value: saltAmount.current.value,
-								unit: saltUnit.current.value
+								value: 0,
+								unit: 'unit'
 							}
 						},
 						main: [
 							{
 								amount: {
-									value: mainValue.current.value,
-									unit: mainUnit.current.value
+									value: 0,
+									unit: 'unit'
 								},
-								name: mainName.current.value
+								name: ''
 							}
 						],
 						other: [
 							{
 								amount: {
-									value: otherValue.current.value,
-									unit: otherUnit.current.value
+									value: 0,
+									unit: 'unit'
 								},
-								name: otherName.current.value
+								name: ''
 							}
 						]
 					}
-				})
-			});
-			setlactoFerments([data, ...lactoferments]);
-		} catch (error) {
-			console.error(error);
-		} finally {
-			window.location.assign('/');
-		}
-	};
-	return (
-		<div className="newLactofermentForm">
-			<h1> New Fermentaion</h1>
+				}}
+				onSubmit={(values, {}) => {
+					alert(JSON.stringify(values));
+				}}
+			>
+				{({
+					values,
+					errors,
+					touched,
+					handleChange,
+					handleBlur,
+					handleSubmit
+				}) => (
+					<form onSubmit={handleSubmit}>
+						<label> Name:</label>
+						<input
+							type="string"
+							name="name"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.name}
+						/>
+						<label> Description:</label>
+						<input
+							type="string"
+							name="description"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.description}
+						/>
+						<label> Volume:</label>
+						<input
+							type="float"
+							name="volume.value"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.volume.value}
+						/>
+						<input
+							type="string"
+							name="volume.unit"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.volume.unit}
+						/>
+
+						<label> Ferment </label>
+						<label> temp </label>
+						<input
+							type="string"
+							name="method.ferment.temp"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.method.ferment.temp}
+						/>
+						<label> duration </label>
+						<input
+							type="string"
+							name="method.ferment.duration"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.method.ferment.duration}
+						/>
+
+						<button type="submit">Submit</button>
+					</form>
+				)}
+			</Formik>
+		</div>
+		// );
+	);
+}
+{
+	/* /* <h1> New Fermentaion</h1>
 			<form onSubmit={handleSubmit}>
 				<label>
 					Recipe Name
@@ -141,7 +257,6 @@ export default function NewLactoFerment(props) {
 						<input type="submit" />
 					</label>
 				</span>
-			</form>
-		</div>
-	);
+			</form> 
+</div> */
 }
