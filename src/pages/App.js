c7424005225af273cb5fa1fc776296bd642b96c0
Michael <michaelodell660@gmail.com>
2021-05-06 16:18:23 -0700

    small css tweaks added icon key on home page

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { GiBeerBottle, GiBubblingFlask } from 'react-icons/gi';
+import { MdList, MdAddBox, MdHome } from 'react-icons/md';
+import { IconContext } from 'react-icons/lib';
 
 export default function App(props) {
 	return (
@@ -9,6 +12,32 @@ export default function App(props) {
 					Welcome to Fermenter, a recipe managment application for home brewers
 					& fermenters.
 				</p>
+				<IconContext.Provider value={{ className: 'react-icons' }}>
+					<span className="center">
+						<ul>
+							<li>
+								{' '}
+								<GiBeerBottle />
+								<MdAddBox /> New beer recipe{' '}
+							</li>
+							<li>
+								{' '}
+								<GiBubblingFlask />
+								<MdAddBox /> New lactic acid fermentation recipe{' '}
+							</li>
+							<li>
+								{' '}
+								<GiBeerBottle />
+								<MdList /> View beer recipes{' '}
+							</li>
+							<li>
+								{' '}
+								<GiBubblingFlask />
+								<MdList /> View lactic acid fermentation recipes{' '}
+							</li>
+						</ul>
+					</span>
+				</IconContext.Provider>
 			</div>
 		</div>
 	);
